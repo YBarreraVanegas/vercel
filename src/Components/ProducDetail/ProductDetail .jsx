afd6155aa5a8b6d6f5340fd8c81105e80df6efd2
YBarreraVanegas <yojansmithbarrera@gmail.com>
2024-03-17 12:44:44 -0500

    q

--- src/Components/ProducDetail/ProductDetail .jsx
+++ src/Components/ProducDetail/ProductDetail .jsx
@@ -1,23 +1,23 @@
-import { useParams } from "react-router-dom";
-import { useGetData } from "../useFetchs";
-import CardDetail from "./CardDetail";
-import ContactDetail from "./ContactDetail";
+import { useParams } from 'react-router-dom'
+import { useGetData } from '../useFetchs'
+import CardDetail from './CardDetail'
+import ContactDetail from './ContactDetail'
 
 const ProductDetail = () => {
-  const { id } = useParams();
-  const url = `${import.meta.env.VITE_URL}/products/${id}`;
+  const { id } = useParams()
+  const url = `${import.meta.env.VITE_URL}/products/${id}`
 
-  const { data, loading, error } = useGetData(url);
+  const { data, loading, error } = useGetData(url)
 
   if (loading) {
-    return <span className="loading">Cargando...</span>;
+    return <span className="loading">Cargando...</span>
   }
 
   if (error) {
-    return <span>Error: {error.message}</span>;
+    return <span>Error: {error.message}</span>
   }
 
-  if (typeof data === "object" && data !== null) {
+  if (typeof data === 'object' && data !== null) {
     return (
       <div>
         <h2 className="h2-detail">Detalles del Producto</h2>
@@ -26,10 +26,10 @@ const ProductDetail = () => {
         </div>
         <ContactDetail />
       </div>
-    );
+    )
   } else {
-    return <span>No se encontraron datos para mostrar.</span>;
+    return <span>No se encontraron datos para mostrar.</span>
   }
-};
+}
 
-export default ProductDetail;
+export default ProductDetail
